ba7566da266172c9da48ef232d26da02bce53b33
Mehdi Izadpanah <meh.izadpanah@gmail.com>
2023-09-09 23:22:07 +0300

    share project

--- PL/Program.cs
+++ PL/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using BL.Data.Model;
+using SH.Data.Model;
 using MudBlazor.Services;
 
 
--- SH/Pages/Error.cshtml.cs
+++ SH/Pages/Error.cshtml.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
-namespace PL.Pages
+namespace SH.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
