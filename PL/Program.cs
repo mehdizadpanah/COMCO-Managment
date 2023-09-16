517cd989435880c3d6561f5849245da5ea077588
Mehdi Izadpanah <meh.izadpanah@gmail.com>
2023-09-16 08:03:46 +0300

    #

--- PL/Program.cs
+++ PL/Program.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SH.Data.Model;
 using MudBlazor.Services;
-
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.Components.Authorization;
+using SH.Class;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,10 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddMudServices();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+//builder.Services.AddAuthentication()
+//    .AddCookie(option =>
+//    option.LoginPath = "/SH/Pages/Identity/Login");
 
 
 var app = builder.Build();
@@ -25,6 +31,9 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
