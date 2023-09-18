002e8fe1581ea099362895a389cf5e79403b00c7
Mehdi Izadpanah <meh.izadpanah@gmail.com>
2023-09-18 21:15:56 +0300

    Login

--- PL/Program.cs
+++ PL/Program.cs
@@ -1,10 +1,9 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using SH.Data.Model;
 using MudBlazor.Services;
-using System.Threading.RateLimiting;
 using Microsoft.AspNetCore.Components.Authorization;
 using SH.Service;
+using Blazored.LocalStorage;
+using MudBlazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +11,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddMudServices();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
-//builder.Services.AddAuthentication()
-//    .AddCookie(option =>
-//    option.LoginPath = "/SH/Pages/Identity/Login");
+builder.Services.AddMudServices(config =>
+{
+    
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
 
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+
+builder.Services.AddBlazoredLocalStorage();
 
 var app = builder.Build();
 
--- SH/Service/CustomAuthenticationStateProvider.cs
+++ SH/Service/CustomAuthenticationStateProvider.cs
@@ -5,27 +5,77 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Microsoft.VisualBasic;
+using SH.Data.Model;
+using System.Security.Principal;
 
 namespace SH.Service
 {
     public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+
     {
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        public ILocalStorageService LocalStorageService { get; set; }
+
+        public CustomAuthenticationStateProvider(ILocalStorageService localStorageService)
         {
+            LocalStorageService = localStorageService;
+            
+
+        }
+
+       
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+           
             var identity = new ClaimsIdentity();
             var user = new ClaimsPrincipal(identity);
+            var username = await LocalStorageService.GetItemAsync<string>("user");
+            var expireTime = await LocalStorageService.GetItemAsync<DateTime>("expireTime");
+            if (username != null && expireTime > DateTime.UtcNow)
+            {
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, username),
+                }, "custom");
+                user = new ClaimsPrincipal(identity);
+            }
+            else
+            {
+                await Logout();
+            }
+            return await Task.FromResult(new AuthenticationState(user));
 
-            return Task.FromResult(new AuthenticationState(user));
-            
         }
-        public void UserAuthenticate(string username)
+
+        public async Task<bool> Login(string username, string password, bool rememberMe)
         {
+            var validate = false || username == "mi" && password == "1234";
+
+            if (!validate) return validate;
+
+            var expireTime = rememberMe ? DateTime.UtcNow.AddDays(30) : DateTime.UtcNow.AddMinutes(30);
             var identity = new ClaimsIdentity(new[]
-           {
-                new Claim(ClaimTypes.Name,username),
-            }, "apiauth_api");
+            {
+                new Claim(ClaimTypes.Name, username),
+            }, "custom");
             var user = new ClaimsPrincipal(identity);
             NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+            await LocalStorageService.SetItemAsync("user", username);
+            await LocalStorageService.SetItemAsync("expireTime", expireTime);
+
+            return validate;
+        }
+
+        public async Task Logout()
+        {
+            await LocalStorageService.RemoveItemAsync("user");
+            await LocalStorageService.RemoveItemAsync("expireTime");
+            var identity = new ClaimsIdentity();
+            var user = new ClaimsPrincipal(identity);
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+
         }
     }
 }
