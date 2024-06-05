072f4bf85e6e2314d7d297268e912a31cb8ac8d1
Mehdi Izadpanah <meh.izadpanah@gmail.com>
2024-06-05 10:06:36 +0300

    Token view model updated wirh Name and Family

--- SH/Data/ModelVM/Authentication/Token.cs
+++ SH/Data/ModelVM/Authentication/Token.cs
@@ -5,5 +5,7 @@
         public string Value { get; set; }
         public DateTime ExpiryDate { get; set; }
         public string UserID { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
     }
 }
